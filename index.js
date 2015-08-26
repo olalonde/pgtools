7e2334c2f91c05d002faba03f45f8ffa8b471496
Fernando Migliorini Tenório <dotenorio@gmail.com>
2015-08-26 17:42:46 -0300

    optimize the callback when exist an error

--- index.js
+++ index.js
@@ -3,28 +3,39 @@ var Client = pg.Client;
 
 var errNames = {
   '42P04': 'duplicate_database',
-  '3D000': 'invalid_catalog_name'
+  '3D000': 'invalid_catalog_name',
+  '28000': 'connection_error'
 };
 
+function error(pgErr) {
+  var err = new Error();
+  err = {
+    name: errNames[pgErr.code],
+    pgErr: pgErr
+  };
+  return err;
+}
+
 function createOrDropDatabase(action) {
+  var err;
   action = action.toUpperCase();
   return function (config, dbName, cb) {
     config.database = 'postgres';
     var client = new Client(config);
     //disconnect client when all queries are finished
     client.on('drain', client.end.bind(client));
-    client.on('error', function () {});
+    client.on('error', function (pgErr) {
+      if (pgErr) {
+        err = error(pgErr);
+        cb(err);
+      }
+    });
     client.connect();
 
     var escapedDbName = dbName.replace(/\"/g, '""');
     client.query(action + ' DATABASE "' + escapedDbName + '"', function (pgErr, res) {
-      var err;
       if (pgErr) {
-        err = new Error();
-        err = {
-          name: errNames[pgErr.code],
-          pgErr: pgErr
-        };
+        err = error(pgErr);
       }
       cb(err, res);
     });
